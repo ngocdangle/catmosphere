c49b77f17f1f35871c0ef6714b81708b189424ed
Ngoc <lengockhanhdang@gmail.com>
2025-03-14 17:44:36 +0100

    change

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function showSidebar(){
+  const sidebar = document.querySelector('.menu-nav');
+  sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.menu-nav');
+  sidebar.style.display = 'none';
+}
